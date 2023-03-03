f25bd5c4a3223fc7e28506691a80b37e63e9924e
Maxim Ziegler <maw0805@outlook.de>
2023-03-03 15:21:28 +0100

    Added to get users input

--- src/com/hbv/LottoApp.java
+++ src/com/hbv/LottoApp.java
@@ -1,4 +1,49 @@
 package com.hbv;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class LottoApp {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        System.out.println("Welche Tippreihe soll generiert werden?");
+        System.out.println("\n1. 6aus49");
+        System.out.println("2. Eurojackpot");
+        System.out.print("\nDeine Auswahl: ");
+
+        String input = reader.readLine();
+
+        if (input.equalsIgnoreCase("6aus49")) {
+            System.out.println("Ausgewaehlt: 6aus49");
+        } else if (input.equalsIgnoreCase("Eurojackpot")) {
+            System.out.println("Ausgewaehlt: Eurojackpot");
+        } else if (input.isEmpty()) {
+            System.out.println("Ausgewaehlt: 6aus49");
+        } else {
+            boolean isValid = false;
+            while (!isValid) {
+                System.out.println("*** UNGÜLTIGE AUSWAHL! ***");
+                System.out.println("Welche Tippreihe soll generiert werden?");
+                System.out.println("\n1. 6aus49");
+                System.out.println("2. Eurojackpot");
+                System.out.print("\nDeine Auswahl: ");
+
+                input = reader.readLine();
+
+                if (input.equalsIgnoreCase("6aus49")) {
+                    isValid = true;
+                    System.out.println("Ausgewaehlt: 6aus49");
+                } else if (input.equalsIgnoreCase("Eurojackpot")) {
+                    isValid = true;
+                    System.out.println("Ausgewaehlt: Eurojackpot");
+                } else if (input.isEmpty()) {
+                    isValid = true;
+                    System.out.println("Ausgewaehlt: 6aus49");
+                }
+            }
+        }
+    }
 }
