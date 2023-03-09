e9825dfc0a904683cdc920420ef39af3d07249ba
Maxim Ziegler <MAW0805@outlook.de>
2023-03-09 18:51:01 +0100

    Fixed some bugs at generating numbers

--- src/com/hbv/lottery/Lotto.java
+++ src/com/hbv/lottery/Lotto.java
@@ -5,6 +5,7 @@ import com.hbv.Generator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Lotto implements Generator {
 
@@ -13,7 +14,6 @@ public class Lotto implements Generator {
 
     public void start() throws IOException {
         askForUnluckyNumbers();
-        sortArray(numbers);
         getArray(numbers);
     }
 
@@ -25,19 +25,7 @@ public class Lotto implements Generator {
         System.out.println("\n---------------------------------------------");
     }
 
-    public void sortArray(int[] arr) {
-        for(int i = 0; i < arr.length; ++i) {
-            for(int j = i + 1; j < arr.length - 1; ++j) {
-                if(arr[i] > arr[j]) {
-                    int tmp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = tmp;
-                }
-            }
-        }
-    }
-
-    public boolean askForUnluckyNumbers() throws IOException {
+    public void askForUnluckyNumbers() throws IOException {
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
 
@@ -49,62 +37,73 @@ public class Lotto implements Generator {
         String input = reader.readLine();
 
         if(input.equalsIgnoreCase("Ja")) {
-            System.out.println("---------------------------------------------");
-            System.out.println("Wie viele Zahlen moechtest du ausschließen?");
-            System.out.println("---------------------------------------------");
-            System.out.print("Deine Auswahl: ");
-
-            int input2 = Integer.parseInt(reader.readLine());
-
-            while(input2 < 0 ||input2 > 6) {
-                System.out.println("---------------------------------------------");
-                System.out.println("Zahl nicht gueltig (0 bis 6)");
-                System.out.println("---------------------------------------------");
-                System.out.print("Deine Auswahl: ");
-                input2 = Integer.parseInt(reader.readLine());
-            }
-            addUnluckyNumbers(input2);
+            addUnluckyNumbers();
         } else if (input.equalsIgnoreCase("Nein")) {
             generateNumbers(false);
         } else {
+            System.out.println("---------------------------------------------");
             System.out.println("Ungueltige Antwort!");
+            System.out.println("---------------------------------------------");
         }
-        return false;
     }
 
-    public void addUnluckyNumbers(int count) throws IOException {
+    public void addUnluckyNumbers() throws IOException {
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
-        for(int i = 0; i < count; ++i) {
+
+        System.out.println("---------------------------------------------");
+        System.out.println("Wie viele Zahlen moechtest du ausschließen?");
+        System.out.println("---------------------------------------------");
+        System.out.print("Deine Auswahl: ");
+
+        int count = Integer.parseInt(reader.readLine());
+
+        while(count < 0 || count > 6) {
+            System.out.println("---------------------------------------------");
+            System.out.println("Zahl nicht gueltig! (0 bis 6)");
+            System.out.println("---------------------------------------------");
+            System.out.print("Deine Auswahl: ");
+
+            count = Integer.parseInt(reader.readLine());
+        }
+
+        for(int i = 0; i < count; i++) {
             System.out.print(i + 1 + ". Zahl: ");
             unluckyNumbers[i] = Integer.parseInt(reader.readLine());
         }
         generateNumbers(true);
     }
 
-    @Override
-    public int[] generateNumbers(boolean unluckyNumbers) {
-        if(!unluckyNumbers) {
-            for (int i = 0; i < 6; ++i) {
-                numbers[i] = (int) (Math.random() * 49);
-            }
-            for(int i = 0; i < numbers.length; ++i) {
-                for(int j = i + 1; j < numbers.length; ++j) {
-                    while(numbers[i] == numbers[j]) {
-                        numbers[i] = (int) (Math.random() * 49);
-                    }
-                }
-            }
-            return numbers;
-        } else {
-            for(int i = 0; i < numbers.length; ++i) {
-                for(int j = 0; j < this.unluckyNumbers.length; ++j) {
-                    while(numbers[i] == this.unluckyNumbers[j]) {
-                        numbers[i] = (int) (Math.random() * 49);
-                    }
+    public void checkArrayValues() {
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = i + 1; j < numbers.length; j++) {
+                while(numbers[i] == numbers[j]) {
+                    numbers[i] = (int) (Math.random() * 49);
                 }
             }
         }
+        Arrays.sort(numbers);
+    }
+
+    @Override
+    public int[] generateNumbers(boolean unluckyNumbers) {
+        if (unluckyNumbers) {
+            for(int i = 0; i < 6; i++) {
+                numbers[i] = (int) (Math.random() * 49);
+            }
+            for(int i = 0; i < numbers.length; i++) {
+                for(int j = 0; j < 6; j++) {
+                    if(numbers[i] == this.unluckyNumbers[j]) {
+                        numbers[i] = (int) (Math.random() * 49);
+                    }
+                }
+            }
+        } else {
+            for(int i = 0; i < 6; i++) {
+                numbers[i] = (int) (Math.random() * 49);
+            }
+        }
+        checkArrayValues();
         return numbers;
     }
 }
